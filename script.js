4fa93077640a634207f3661bbd64bc5879847a05
nteferra <n.teferra@yahoo.com>
2023-05-27 13:47:10 -0400

    add basic calculations and operation variables

---
+++ script.js
@@ -0,0 +1,12 @@
+const calc = {
+    add: (a,b) => a + b,
+    subtract: (a,b) => a - b,
+    multiply: (a,b) => a * b,
+    divide: (a,b) => a / b, 
+}
+
+const op = {
+    first: 0,
+    second: 0,
+    erator: "",
+}
